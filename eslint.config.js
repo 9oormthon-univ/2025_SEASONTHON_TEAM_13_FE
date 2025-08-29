7630f8a1d070fa99a709e5ee1c78fee7469ef0a7
Helloyunho <me@helloyunho.xyz>
2025-08-29 21:39:40 +0900

    fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import js from '@eslint/js';
 import globals from 'globals';
@@ -27,4 +27,4 @@ export default tseslint.config([
   ...neostandard({
     semi: true
   })
-], storybook.configs["flat/recommended"]);
+], storybook.configs['flat/recommended']);
