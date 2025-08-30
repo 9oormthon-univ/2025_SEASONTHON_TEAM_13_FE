87dae396839546eb012b81927d5061e31191982e
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-31 01:51:44 +0900

    init: 디자인시스템 폰트

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,14 @@ function App () {
     <>
       <p className='text-2xl font-bold'>tailwindcss test</p>
       <img src={reactLogo} className='size-4' alt='React logo' />
-      <p className='text-gray100'>123</p>
+      <p className='heading1'>heading1</p>
+      <p className='heading2'>heading2</p>
+      <p className='heading3'>heading3</p>
+      <p className='body-xl'>body-xl</p>
+      <p className='body-l'>body-l</p>
+      <p className='body-m'>body-m</p>
+      <p className='body-s'>body-s</p>
+      <p className='body-xs'>body-xs</p>
     </>
   );
 }
