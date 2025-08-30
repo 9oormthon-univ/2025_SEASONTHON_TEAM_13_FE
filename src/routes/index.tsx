0fce86f6bfa888d88b03528ca19642a282c40be3
chahyunwoo00 <ckgusdn8@naver.com>
2025-08-30 22:23:45 +0900

    init: 디자인 색상 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ function App () {
     <>
       <p className='text-2xl font-bold'>tailwindcss test</p>
       <img src={reactLogo} className='size-4' alt='React logo' />
+      <p className='text-gray100'>123</p>
     </>
   );
 }
