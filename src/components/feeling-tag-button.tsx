80a202369f0aa88fb3ee6ce9646f716162a4ead6
Helloyunho <me@helloyunho.xyz>
2025-09-01 00:41:45 +0900

    feat: add `selected` prop
    chore: resize button based on the design

--- src/components/feeling-tag-button.tsx
+++ src/components/feeling-tag-button.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 
-export const FeelingTagButton = ({ className, ...props }: React.ComponentProps<'button'>) => {
+export const FeelingTagButton = ({ className, selected = false, ...props }: React.ComponentProps<'button'> & {
+  selected: boolean
+}) => {
   return (
-    <button className={cn('inline-flex rounded-full border-1 border-[#E6E6E6] px-3 py-1 text-sm hover:cursor-pointer', className)} {...props} />
+    <button
+      className={cn(
+        'inline-flex rounded-full border-1 border-[#E6E6E6] px-6 py-2 text-sm font-medium hover:cursor-pointer transition-colors',
+        selected && 'border-primary bg-[#FFEBEA] text-primary',
+        className
+      )} {...props}
+    />
   );
 };
