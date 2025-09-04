b06e1ab50c3e56b722c4a1387f3f74a87474fc8e
chahyunwoo00 <ckgusdn8@naver.com>
2025-09-04 17:15:56 +0900

    refactor: 리다이렉트 동적으로 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -33,7 +33,13 @@ function App () {
         <button
           className='w-full p-[20px] bg-[#FEE500] rounded-[10px] text-gray800 text-[16px] font-bold leading-[140%] cursor-pointer flex items-center justify-center relative'
           onClick={() => {
-            window.location.href = 'https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fresponse_type%3Dcode%26client_id%3Da791581bd3216a3c1fb275307a6310ff%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A5173%252Foauth%252Fcallback%26scope%3Daccount_email%252Cprofile_nickname%252Cprofile_image%26through_account%3Dtrue#login';
+            // 현재 호스트를 기준으로 동적 redirect_uri 생성
+            const currentHost = window.location.origin;
+            const redirectUri = `${currentHost}/oauth/callback`;
+            const encodedRedirectUri = encodeURIComponent(redirectUri);
+            const kakaoAuthUrl = `https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fresponse_type%3Dcode%26client_id%3Da791581bd3216a3c1fb275307a6310ff%26redirect_uri%3D${encodedRedirectUri}%26scope%3Daccount_email%252Cprofile_nickname%252Cprofile_image%26through_account%3Dtrue#login`;
+
+            window.location.href = kakaoAuthUrl;
           }}
         >
           <img src={kakao} alt='카카오' className='absolute left-[20px] w-[18px] h-[18px]' />
