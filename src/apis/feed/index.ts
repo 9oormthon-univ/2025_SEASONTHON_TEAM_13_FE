288f876b58b3e259cbef1c4dcf4d4c360ac9b66e
Helloyunho <me@helloyunho.xyz>
2025-09-06 22:13:16 +0900

    fix: use utc time

--- src/apis/feed/index.ts
+++ src/apis/feed/index.ts
@@ -46,9 +46,9 @@ export const postFeedComment = async (postId: number, content: string) => {
 
 export const getMyTodayFeed = async (): Promise<Feed | null> => {
   const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
+  const year = now.getUTCFullYear();
+  const month = String(now.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(now.getUTCDate()).padStart(2, '0');
   const date = `${year}-${month}-${day}`;
   const response = await instance.get('/posts/me', {
     params: {
