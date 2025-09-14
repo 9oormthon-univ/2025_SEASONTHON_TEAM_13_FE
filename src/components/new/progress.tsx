335cbcca222b6833453e394e7c150a64342d87d0
Helloyunho <me@helloyunho.xyz>
2025-09-15 00:16:44 +0900

    fix: forgot to add original props

--- src/components/new/progress.tsx
+++ src/components/new/progress.tsx
@@ -1,6 +1,9 @@
-export const Progress = ({ selectedIndex = 0 }: { selectedIndex: 0 | 1 | 2 | 3 }) => {
+import type React from 'react';
+import { cn } from '@/lib/utils';
+
+export const Progress = ({ selectedIndex = 0, className, ...props }: React.ComponentProps<'div'> & { selectedIndex: 0 | 1 | 2 | 3 }) => {
   return (
-    <div className='flex flex-row gap-1.5'>
+    <div className={cn('flex flex-row gap-1.5', className)} {...props}>
       {[0, 1, 2, 3].map((i) => (
         <div key={i} className={`h-2 rounded-full ${i === selectedIndex ? 'bg-primary w-7' : 'bg-gray300 w-2'}`} />
       ))}
